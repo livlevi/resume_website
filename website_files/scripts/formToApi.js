8272b5047e2e460cd51833b422dd3366a38ec125
livlevi <91070792+livlevi@users.noreply.github.com>
2022-05-04 14:14:27 -0400

    Fixed SMS issue, since the function had a null value when referencing the sms variable, there was an error. Reference to SMS was removed.

--- website_files/scripts/formToApi.js
+++ website_files/scripts/formToApi.js
@@ -1,12 +1,14 @@
-function formToApi(event) {
+function formToApi(event, typeOfSending) {
+
     event.preventDefault()
-    
+
     var data = {
+        typeOfSending: typeOfSending,
         destinationEmail: document.getElementsByName('email')[0].value,
         message: document.getElementsByName('message')[0].value
     }
 
-    fetch( "https://pldjrzsr1g.execute-api.us-east-1.amazonaws.com/sendingStage/sending", {
+    fetch( "https://xjn7t5jrnf.execute-api.us-east-1.amazonaws.com/prod/sending" , {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -14,6 +16,4 @@ function formToApi(event) {
         body: JSON.stringify(data),
         mode: "no-cors"
     })
-
-
 }
